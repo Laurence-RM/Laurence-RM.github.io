70cd918f5fb27fa282b53da72716cb39d1d511b4
Laurence Mullen <laurencemullen99@gmail.com>
2022-06-28 22:15:17 -0400

    New style, expando change

--- js/main.js
+++ js/main.js
@@ -1,14 +1,11 @@
 function expandProject(project) {
     var element = document.getElementById(project);
-    var chevron_down = document.getElementsByClassName("fa-chevron-down");
-    var chevron_up = document.getElementsByClassName("fa-chevron-up");
-    if (element.style.display == "none") {
-        element.style.display = "inline-block";
-        chevron_down[0].style.display = "none";
-        chevron_up[0].style.display = "inline";
+    var chevron_down = document.getElementById(project+"_chev");
+    if (element.style.height != "300px") {
+        element.style.height = "300px";
+        chevron_down.style.transform = "rotateX(180deg)";
     } else {
-        element.style.display = "none";
-        chevron_down[0].style.display = "inline";
-        chevron_up[0].style.display = "none";
+        element.style.height = "5px";
+        chevron_down.style.transform = "rotateX(0deg)";
     }
 }
