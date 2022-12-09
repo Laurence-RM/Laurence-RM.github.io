eec9ef6761a57df35b071a48abc7ebd068a56229
Laurence Mullen <laurencemullen99@gmail.com>
2022-12-09 17:03:31 -0500

    Center content, static navmenu, fix css bugs

--- js/main.js
+++ js/main.js
@@ -1,13 +1,21 @@
 function expandProject(project) {
     var element = document.getElementById(project);
     var chevron_down = document.getElementById(project+"_chev");
-    if (element.scrollHeight == 0) {
-        chevron_down.style.transform = (!chevron_down.style.transform || chevron_down.style.transform =="rotate(0deg)") ? "rotate(360deg)" : "rotate(0deg)";
-    } else if (element.style.height != element.scrollHeight+"px") {
-        element.style.height = element.scrollHeight+"px";
-        chevron_down.style.transform = "rotateX(180deg)";
-    } else {
-        element.style.height = "0px";
+    element.classList.toggle('closed');
+    console.log(chevron_down.style.transform);
+    if (chevron_down.style.transform == "rotateX(180deg)") {
         chevron_down.style.transform = "rotateX(0deg)";
+    } else {
+        chevron_down.style.transform = "rotateX(180deg)";
     }
+
+    // if (element.scrollHeight == 0) {
+    //     chevron_down.style.transform = (!chevron_down.style.transform || chevron_down.style.transform =="rotate(0deg)") ? "rotate(360deg)" : "rotate(0deg)";
+    // } else if (element.style.max-height == "0px") {
+    //     element.style.maxHeight = "1000px";
+    //     chevron_down.style.transform = "rotateX(180deg)";
+    // } else {
+    //     element.style.maxHeight = "0px";
+    //     chevron_down.style.transform = "rotateX(0deg)";
+    // }
 }
